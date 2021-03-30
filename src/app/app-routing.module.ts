9556630e5862ac85142a976bc38110af010870f9
Vatsal Shah <vashah@teksystems.com>
2021-03-30 10:28:28 +0530

    added home page route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomepageComponent } from './homepage/homepage.component';
 import { SigninComponent } from './signin/signin.component';
 import { SignupComponent } from './signup/signup.component';
 
@@ -10,6 +11,12 @@ const routes: Routes = [
   },
   {
     path: "signup", component: SignupComponent
+  },
+  {
+    path: "home", component: HomepageComponent
+  },
+  {
+    path: "", component: HomepageComponent
   }
 ];
 
