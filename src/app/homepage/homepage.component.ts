4b244160f9d1f21517e072ab2869c736ce8cbec1
Vatsal Shah <vashah@teksystems.com>
2021-03-30 17:51:42 +0530

    logic for searching of food item

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-homepage',
@@ -9,6 +9,46 @@ export class HomepageComponent implements OnInit {
 
   constructor() { }
 
+  searchBar: any;
+  searchResult: any;
+  productFound!: boolean;
+  p_img: any;
+  p_name!: string;
+  p_desc!: string;
+  p_price!: number;
+  @Input() searchMessage!: string;
+  showmsg!: boolean;
+
   ngOnInit(): void {
+  }
+
+  handleSearch(){
+    this.showmsg = true;
+    this.searchBar= document.getElementById("searchBar");
+    this.searchResult= document.getElementById("searchResult");
+    console.log("Searched Item: " + this.searchBar.value);
+    if(this.searchProduct(this.searchBar.value)){
+      this.searchResult.style.display = 'block';
+      this.searchMessage = "Food Item found";
+    } else{
+      this.searchMessage = "Sorry Food Item not found";
+      this.searchResult.style.display = 'none';
+    }
+  }
+
+  searchProduct(name: string): boolean{
+    for(let i=0; i<localStorage.length; i++){
+      var productId: any = localStorage.key(i);
+      var prod = localStorage.getItem(productId);
+      var product: any = prod?.split("+");
+      if(product[1].toLowerCase() == name.toLowerCase()){
+        this.p_img = product[0];
+        this.p_name = product[1];
+        this.p_desc = product[2];
+        this.p_price = product[3];
+        return true;
+      }
+    }
+    return false;
   }
 }
